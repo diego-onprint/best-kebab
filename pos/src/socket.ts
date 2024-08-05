c08b53d73abda58175573b9626c45edd57ee3089
diego <diegoeliseoiovane@gmail.com>
2024-08-05 09:54:40 -0300

    fix: production endpoints

--- pos/src/socket.ts
+++ pos/src/socket.ts
@@ -1,9 +1,9 @@
 import { io } from "socket.io-client"
 
 // Prod
-// const URL = "https://best-kebab-server.smart-pos.ch/"
+const URL = "https://best-kebab-server.smart-pos.ch/"
 
 // Dev
-const URL = "http://localhost:8108"
+// const URL = "http://localhost:8108"
 
 export default io(URL)
--- server/db/connection.js
+++ server/db/connection.js
@@ -3,24 +3,24 @@ import pkg from "pg"
 const { Pool } = pkg
 
 // PROD
-// export const pool = new Pool({
-//     host: "some-postgres",
-//     user: "postgres",
-//     database: "bestkebab",
-//     password: "postgres",
-//     port: 5432,
-//     allowExitOnIdle: true,
-// })
-
-// DEV
 export const pool = new Pool({
-    host: "localhost",
+    host: "some-postgres",
     user: "postgres",
-    database: "bestKebab",
+    database: "bestkebab",
     password: "postgres",
     port: 5432,
     allowExitOnIdle: true,
 })
+
+// DEV
+// export const pool = new Pool({
+//     host: "localhost",
+//     user: "postgres",
+//     database: "bestKebab",
+//     password: "postgres",
+//     port: 5432,
+//     allowExitOnIdle: true,
+// })
 
 try {
     await pool.query("SELECT NOW()")
