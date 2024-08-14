d71fbd26345b2fa5319984e800fef35799fef926
diego <diegoeliseoiovane@gmail.com>
2024-08-14 08:00:19 -0300

    .

--- shop/src/views/checkout/Checkout.tsx
+++ shop/src/views/checkout/Checkout.tsx
@@ -1,6 +1,11 @@
+import { useEffect } from 'react'
 import CheckoutForm from '../../components/checkout_form/CheckoutForm'
 
 const Checkout = () => {
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
 
   return (
     <div className="p-4">
